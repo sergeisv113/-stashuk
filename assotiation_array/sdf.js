eff3234dfe68762cba7beff7d606dc1ab0a956b0
Sergei Strilets <sergeisv113@gmail.com>
2022-07-20 22:24:20 +0300

    commit-1

--- assotiation_array/sdf.js
+++ assotiation_array/sdf.js
@@ -1,3 +1,5 @@
 function findMultiples(int,limit){
     return Array(Math.floor(limit/int)).fill(1).map((x,i)=>int*(i+1));
 }
+const playerRankUp = points =>
+    points >= 100 ? 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.' : false;
